9ffe2d1518f9794ba48a4f8c61fc7b690655f56f
Ammar Shahzad <sp24-bse-012@cuilahore.edu.pk>
2026-04-08 01:31:43 +0500

    Add script for reading CSV and Excel files using pandas

---
+++ Pandas/lec 2.py
@@ -0,0 +1,7 @@
+import pandas as pd 
+df = pd.read_csv('C:\\Users\\Eliminator 105\\Desktop\\Book1.csv')
+print(df,"\n\n")
+
+df = pd.read_excel("C:\\Users\\Eliminator 105\\Desktop\\nna.xlsx","Sheet2")
+print(df)
+
