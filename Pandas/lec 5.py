867d975cc7ad3bb80026d382ae566825fd45669a
Ammar Shahzad <sp24-bse-012@cuilahore.edu.pk>
2026-04-08 01:32:33 +0500

    ..

---
+++ Pandas/lec 5.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+df = pd.read_excel("C:\\Users\\Eliminator 105\\Desktop\\nna.xlsx","Sheet2")
+print(df,"\n\n")
+
+#new_df = df.replace(963.0,np.NaN)
+#new_df = df.replace({'section','[1-9]'}," ",regex = True) #remove number or latters form cny columna
+print(new_df)
